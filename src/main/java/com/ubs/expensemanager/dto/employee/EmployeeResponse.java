9e62cbec3a62b465c170fb000e1ebe74e077a8e6
Gui <guidaltrino@gmail.com>
2026-01-10 14:08:24 -0300

    Removing unused imports

--- src/main/java/com/ubs/expensemanager/dto/employee/EmployeeResponse.java
+++ src/main/java/com/ubs/expensemanager/dto/employee/EmployeeResponse.java
@@ -2,9 +2,6 @@ package com.ubs.expensemanager.dto.employee;
 
 import java.util.UUID;
 
-import com.ubs.expensemanager.domain.Department;
-import com.ubs.expensemanager.domain.Employee;
-
 public record EmployeeResponse(
 		    UUID id,
 		    String name,
--- src/main/java/com/ubs/expensemanager/dto/expense/ExpenseResponse.java
+++ src/main/java/com/ubs/expensemanager/dto/expense/ExpenseResponse.java
@@ -3,7 +3,6 @@ package com.ubs.expensemanager.dto.expense;
 import java.util.UUID;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import com.ubs.expensemanager.domain.Category;
 import com.ubs.expensemanager.domain.enums.ExpenseStatus;
 
 public record ExpenseResponse(
