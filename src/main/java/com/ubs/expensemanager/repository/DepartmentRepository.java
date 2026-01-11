0d66acb3c3a2f8d218609b1b63d2e274f7cf9473
Gui <guidaltrino@gmail.com>
2026-01-11 19:05:53 -0300

    Removing findByEmail method

--- src/main/java/com/ubs/expensemanager/repository/DepartmentRepository.java
+++ src/main/java/com/ubs/expensemanager/repository/DepartmentRepository.java
@@ -1,15 +1,12 @@
 package com.ubs.expensemanager.repository;
 
 import com.ubs.expensemanager.domain.Department;
-import com.ubs.expensemanager.domain.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, UUID> {
-	Optional<Employee> findByEmail(String email);
 }
